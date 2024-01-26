32585f0f572daa9aa925e4842a22f96ae9cbfe5a
mutthuappalaraju <arappalaraju@gmail.com>
2024-01-26 17:55:08 +0530

    frist roboshop shell

--- 02-catalogue.sh
+++ 02-catalogue.sh
@@ -13,7 +13,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo "script start executing $TIMESTAMP"  &>> $LOGFILE
 
 VALIDATE(){
-    if [ $1 -ne o ]
+    if [ $1 -ne 0 ]
     then 
         echo -e "$2...$R failure $N"
         exit 1
